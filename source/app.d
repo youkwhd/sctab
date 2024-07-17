dca57873cfacf503152eddeed75e9c3fea34063f
youkwhd <lolywk@tutanota.com>
2024-07-17 16:01:31 +0700

    chore: kinda bad, add overwrite filename

--- source/app.d
+++ source/app.d
@@ -10,10 +10,12 @@ immutable cacheDir = "cache/";
 
 void fetch()
 {
-    string[] urls = [
-        "https://raw.githubusercontent.com/torvalds/linux/master/arch/x86/entry/syscalls/syscall_64.tbl",
-        "https://raw.githubusercontent.com/torvalds/linux/master/arch/x86/entry/syscalls/syscall_32.tbl",
-        "https://raw.githubusercontent.com/torvalds/linux/master/include/linux/syscalls.h",
+    auto urls = [
+        /// <url>, <filename overwrite>
+        ["https://raw.githubusercontent.com/torvalds/linux/master/arch/x86/entry/syscalls/syscall_64.tbl",    null],
+        ["https://raw.githubusercontent.com/torvalds/linux/master/arch/x86/entry/syscalls/syscall_32.tbl",    null],
+        ["https://raw.githubusercontent.com/torvalds/linux/master/include/linux/syscalls.h",                  null],
+        ["https://raw.githubusercontent.com/torvalds/linux/master/include/asm-generic/syscalls.h",            "syscalls-asm-generic.h"],
     ];
 
     if (!fs.exists(cacheDir)) {
@@ -24,7 +26,8 @@ void fetch()
     TODO: add threading, log downloaded file.
     */
     for (size_t i = 0; i < urls.length; i++) {
-        download(urls[i], cacheDir ~ baseName(urls[i]));
+        string filename = urls[i][1] ? urls[i][1] : baseName(urls[i][0]);
+        download(urls[i][0], cacheDir ~ filename);
     }
 }
 
