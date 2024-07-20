38db84b53715de44623bd6da52d871d48efa78e2
youkwhd <lolywk@tutanota.com>
2024-07-20 20:49:52 +0700

    refactor: split generate into functions

--- source/sctab/gen.d
+++ source/sctab/gen.d
@@ -23,74 +23,80 @@ private string[][] rawTable(Arch arch)
     }
 }
 
-void generate(Arch arch, File type)
+private void generateCsv(Arch arch)
 {
     string[][] table = rawTable(arch);
 
+    write("Number,Name");
+
+    for (int i = 0; i < 6; i++)
+        write(",arg" ~ to!(string)(i));
+
+    write("\n");
+
+    foreach (ref row; table)
+    {
+        write(row[0] ~ ",");
+        write(row[2]);
+
+        string func = decl.func("sys_" ~ row[2]);
+        if (func.empty && row.length > 3)
+            func = decl.func(row[3]);
+
+        string[] params = decl.params(func);
+        for (int i = 0; i < 6; i++)
+        {
+            string param = i < params.length ? params[i] : "";
+            write("," ~ param);
+        }
+
+        write("\n");
+    }
+
+}
+
+private void generateHtml(Arch arch)
+{
+    string[][] table = rawTable(arch);
+
+    writeln("<table>");
+    writeln("    <tr>");
+    writeln("        <th>Number</th>");
+    writeln("        <th>Name</th>");
+
+    for (int i = 0; i < 6; i++)
+        writeln("        <th>arg" ~ to!(string)(i) ~ "</th>");
+
+    writeln("    </tr>");
+
+    foreach (ref row; table)
+    {
+        writeln("    <tr>");
+        writeln("        <td>" ~ row[0] ~ "</td>");
+        writeln("        <td>" ~ row[2] ~ "</td>");
+
+        string func = decl.func("sys_" ~ row[2]);
+        if (func.empty && row.length > 3)
+            func = decl.func(row[3]);
+
+        string[] params = decl.params(func);
+        for (int i = 0; i < 6; i++)
+        {
+            string param = i < params.length ? params[i] : "";
+            writeln("        <td>" ~ param ~ "</td>");
+        }
+
+        writeln("    </tr>");
+    }
+
+    writeln("</table>");
+}
+
+void generate(Arch arch, File type)
+{
     final switch (type)
     {
-        case File.html:
-            writeln("<table>");
-
-            writeln("    <tr>");
-            writeln("        <th>Number</th>");
-            writeln("        <th>Name</th>");
-
-            for (int i = 0; i < 6; i++)
-                writeln("        <th>arg" ~ to!(string)(i) ~ "</th>");
-
-            writeln("    </tr>");
-
-            foreach (row; table)
-            {
-                writeln("    <tr>");
-                writeln("        <td>" ~ row[0] ~ "</td>");
-                writeln("        <td>" ~ row[2] ~ "</td>");
-
-                string func = decl.func("sys_" ~ row[2]);
-                if (func.empty && row.length > 3)
-                    func = decl.func(row[3]);
-
-                string[] params = decl.params(func);
-                for (int i = 0; i < 6; i++)
-                {
-                    string param = i < params.length ? params[i] : "";
-                    writeln("        <td>" ~ param ~ "</td>");
-                }
-
-                writeln("    </tr>");
-            }
-
-            writeln("</table>");
-            break;
-
-        case File.csv:
-            write("Number,Name");
-
-            for (int i = 0; i < 6; i++)
-                write(",arg" ~ to!(string)(i));
-
-            write("\n");
-
-            foreach (row; table)
-            {
-                write(row[0] ~ ",");
-                write(row[2]);
-
-                string func = decl.func("sys_" ~ row[2]);
-                if (func.empty && row.length > 3)
-                    func = decl.func(row[3]);
-
-                string[] params = decl.params(func);
-                for (int i = 0; i < 6; i++)
-                {
-                    string param = i < params.length ? params[i] : "";
-                    write("," ~ param);
-                }
-
-                write("\n");
-            }
-
-            break;
+        case File.html: return generateHtml(arch);
+        case File.csv: return generateCsv(arch);
     }
 }
